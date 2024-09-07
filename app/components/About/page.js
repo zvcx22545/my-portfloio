df1cbb86758073513a2c774210023fe8d62b14ab
DRARINS <zvcx1236@gmail.com>
2024-09-07 14:24:44 +0700

    package.json

--- app/components/About/page.js
+++ app/components/About/page.js
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import './About.css'
+import Link from 'next/link'
 function About() {
   return (
     <div className=''>
@@ -10,7 +12,15 @@ function About() {
             </div>
         </div>
         <div className="flex justify-between items-center">
-        <div className="font-semibold text-[1.5rem]">I like to improving about coding.</div>
+        <div>
+          <div className="font-semibold text-[1.5rem]">I like to improving about coding.</div>
+            <button className=" font-semibold text-[1rem] text-white py-2 px-4 rounded-full CV mt-6">
+          <Link download href="./Chisanuponh Limsakul.pdf" passHref className='flex items-center justify-center gap-2 rounded-[12px]'>
+              <img src="./images/play.png" className='w-[50px]' alt="This is logo download of cv" />
+              <div className="font-bold text-white">DOWNLOAD CV</div>
+        </Link>
+            </button>
+        </div>
         <div className="font-semibold text-[1rem]">ผมเป็นที่ชอบเขียนโค๊ดแล้วเห็นการเปลี่ยนแปลง<br />
          หน้าเว็บไซต์ซึ่งทำให้ผมได้แรงบันดาลใจในการเขียนโคํด<br />
           และพัฒนาตัวเองในการทำส่วนของหน้าตาเว็บไซต์ และ<br />
